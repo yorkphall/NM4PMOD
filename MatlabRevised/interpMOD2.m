ce2cfa7711c6f5ac964a8094dc531bf7b9ec72f3
EduardoChomen <63215678+EduardoChomen@users.noreply.github.com>
2020-04-06 11:41:09 -0400

    Update interpMOD2.m
    I have submitted this program for a second time since my first version did not use the original variable temp, as was instructed in the Optional Bonus Homework #12 PDF File.

--- MatlabRevised/interpMOD2.m
+++ MatlabRevised/interpMOD2.m
@@ -1,20 +1,19 @@
-function interpMOD2(x,y,xr)       %MOD
+function interpMOD2(temp,xr)       %MOD
 %% interp - Program to interpolate data using Lagrange 
 % polynomial to fit quadratic to three data points
-% Original by AJG; modified by Eduardo Chomen Ramos 20200405 %MOD
-% interpMOD2([1 2 3],[2 3 4],[0 10])                          %MOD
-% x = x coordinate of the three points to be interpolated    %MOD
-% y = y coordinate of the three points to be interpolated    %MOD
-% xr = range of interpolation (from x_min to x_max)
+% Original by AJG; modified by Eduardo Chomen Ramos 20200406 %MOD
+% interpMOD2([1 2 ; 3 4 ; 5 6 ],[0 10])                         %MOD
+% temp = The three points to be interpolated                    %MOD
+% xr = range of interpolation (from x_min to x_max)             %MOD
 help interp;  % Clear memory and print header
 %% The following modded lines were removed since, now that the program runs from the command line, it does not need to request inputs %MOD
 %% * Initialize the data points to be fit by quadratic  %MOD
 %disp('Enter data points as x,y pairs (e.g., [1 2])');  %MOD
-%for i=1:3                                              %MOD
-%temp = input('Enter data point: ');                    %MOD
-%  x(i) = temp(1);                                      %MOD
-%  y(i) = temp(2);                                      %MOD
-%end                                                    %MOD
+for i=1:3                                             
+%temp = input('Enter data point: ');                   %MOD
+x(i) = temp(i,1);                                      %MOD
+y(i) = temp(i,2);                                      %MOD
+end  
 %% * Establish the range of interpolation (from x_min to x_max)   %MOD
 %xr = input('Enter range of x values as [x_min x_max]: ');        %MOD
 %% * Find yi for the desired interpolation values xi using
